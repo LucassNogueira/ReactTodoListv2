b5c9cb8c734abf4d70cdc64a75a4b32cdb377f2a
LucassNogueira <simplylucasjason@gmail.com>
2022-03-09 13:57:07 -0600

    stopping point for SME meeting

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Form from "./comps/Form";
 import TodoList from "./comps/TodoList";
 
 function App() {
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
   return (
     <div className="App">
       <header>
         <h1>Lucas' Todo List</h1>
       </header>
-      <Form />
-      <TodoList />
+      <Form
+        setInputText={setInputText}
+        setTodos={setTodos}
+        todos={todos}
+        inputText={inputText}
+      />
+      <TodoList setTodos={setTodos} todos={todos} inputText={inputText} />
     </div>
   );
 }
--- src/comps/TodoList.jsx
+++ src/comps/TodoList.jsx
@@ -1,9 +1,13 @@
 import React from "react";
-
-export default function TodoList() {
+import Todo from "./Todo";
+export default function TodoList({ todos, setTodos }) {
   return (
     <div className="todo-container">
-      <ul className="todo-list"></ul>
+      <ul className="todo-list">
+        {todos.map((todo) => (
+          <Todo setTodos={setTodos} text={todo.text} key={todo.id} />
+        ))}
+      </ul>
     </div>
   );
 }
